c25551d1bbddfbb9364e4964160b00b8654c62e5
Santiago José Dandois <santiago.dandois@widergy.com>
2023-08-02 16:54:01 -0300

    remove comment

--- src/permissions-validations.ts
+++ src/permissions-validations.ts
@@ -20,18 +20,6 @@ interface AuthorizeResponse {
   conditionAlternatives?: ConditionAlternatives;
 }
 
-/**
- *
- * Precondition: the scope is related to the resource.
- *
- * @param granted_roles
- * @param resource
- * @param scope
- * @param permissions
- * @returns
- *
- */
-
 export const authorize = (
   granted_roles: string[],
   resource: string,
